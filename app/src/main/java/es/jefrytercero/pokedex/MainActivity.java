175c4d710eb1d6fd4e94cf7fc1a89449a8bf20d6
Alessandro <alessandrogalindo7@gmail.com>
2019-12-12 09:20:40 -0600

    Cambios

--- app/src/main/java/es/jefrytercero/pokedex/MainActivity.java
+++ app/src/main/java/es/jefrytercero/pokedex/MainActivity.java
@@ -20,7 +20,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements ListaPokemonAdapter.ItemClickListener{
 
-    private static final String TAG = " Prueba GitHub - POKEMONAPPDEX";
+    private static final String TAG = " Prueba GitH - POKEMONAPPDEX";
 
 
     private RecyclerView recyclerView;
